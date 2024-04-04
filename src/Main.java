f327123c944dba40f51c57b967c39f6b43df0da1
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-03 23:53:39 -0300

    Add problem.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/*
+ * 1. Programar una clase Persona con atributos suficientes para almacenar nombre,
+ * apellido y edad de una persona.
+ * Crear un programa que permita ingresar los datos de algunas personas por teclado y
+ * con esos datos cree instancias y muestre por pantalla el estado de las mismas.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
